f36653432e1c59aa2d496fede1a1729d03eb9c9b
Dark98 <DarkAidz98@gmail.com>
2019-10-13 01:18:38 +0100

    2006ReBot Update
    Separated commands into own classes
    added a few more simple af commands to make bot a bit more useful

---
+++ 2006Redone Server/src/redone/integrations/discord/commands/Commands.java
@@ -0,0 +1,25 @@
+package redone.integrations.discord.commands;
+
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+
+public class Commands implements MessageCreateListener {
+    @Override
+    public void onMessageCreate(MessageCreateEvent event) {
+        Message message = event.getMessage();
+        if (message.getContent().equalsIgnoreCase("::commands")) {
+            event.getChannel().sendMessage("::forum/::forums"
+                    + System.lineSeparator() +
+                    "::hiscores/::highscores"
+                    + System.lineSeparator() +
+                    "::online"
+                    + System.lineSeparator() +
+                    "::players"
+                    + System.lineSeparator() +
+                    "::vote"
+                    + System.lineSeparator() +
+                    "::website/::site");
+        }
+    }
+}
