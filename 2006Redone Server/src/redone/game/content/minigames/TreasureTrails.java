c14f8477537bea59f028268b6ed131b9550b9204
RedSparr0w <admin@redsparr0w.com>
2019-10-27 21:21:10 +1300

    refactor treasure trails

--- 2006Redone Server/src/redone/game/content/minigames/TreasureTrails.java
+++ 2006Redone Server/src/redone/game/content/minigames/TreasureTrails.java
@@ -7,7 +7,7 @@ public class TreasureTrails {
 
 	public static int lowLevelReward[] = { 1077, 1125, 1165, 1195, 1297, 1367,
 			853, 7390, 7392, 7394, 7396, 7386, 7388, 1099, 1135, 1065, 851 };
-	public static int mediemLevelReward[] = { 1073, 1123, 1161, 1199, 1301,
+	public static int mediumLevelReward[] = { 1073, 1123, 1161, 1199, 1301,
 			1371, 857, 2577, 2579, 2487, 2493, 2499, 2631, 855 };
 	public static int highLevelReward[] = { 1079, 1093, 1113, 1127, 1147, 1163,
 			1185, 1201, 1275, 1303, 1319, 1333, 1359, 1373, 2491, 2497, 2503,
@@ -18,67 +18,78 @@ public class TreasureTrails {
 	public static int lowLevelStacks[] = { 995, 380, 561, 886, };
 	public static int mediumLevelStacks[] = { 995, 374, 561, 563, 890, };
 	public static int highLevelStacks[] = { 995, 386, 561, 563, 560, 892 };
-	public static int allStacks[] = { 995, 380, 561, 886, 374, 561, 563, 890,
-			386, 561, 563, 560, 892 };
+	public static int allStacks[] = { 995, 380, 561, 886, 374, 561, 563, 890, 386, 561, 563, 560, 892 };
 
 	public static void addClueReward(Client c, int clueLevel) {
 		int chanceReward = Misc.random(2);
 		if (clueLevel == 0) {
 			switch (chanceReward) {
 			case 0:
-				displayReward(c, lowLevelReward[Misc.random(16)], 1,
+				displayReward(c,
 						lowLevelReward[Misc.random(16)], 1,
-						lowLevelStacks[Misc.random(3)], 1 + Misc.random(150));
+						lowLevelReward[Misc.random(16)], 1,
+						lowLevelStacks[Misc.random(3)], 1 + Misc.random(150)
+				);
 				break;
 			case 1:
-				displayReward(c, lowLevelReward[Misc.random(16)], 1,
-						lowLevelStacks[Misc.random(3)], 1 + Misc.random(150),
-						-1, 1);
+				displayReward(c,
+						lowLevelReward[Misc.random(16)], 1,
+						lowLevelStacks[Misc.random(3)], 1 + Misc.random(150)
+				);
 				break;
 			case 2:
-				displayReward(c, lowLevelReward[Misc.random(16)], 1, -1, 1, -1,
-						1);
+				displayReward(c, lowLevelReward[Misc.random(16)], 1);
 				break;
 			}
 		} else if (clueLevel == 1) {
 			switch (chanceReward) {
 			case 0:
-				displayReward(c, mediemLevelReward[Misc.random(13)], 1,
-						mediemLevelReward[Misc.random(13)], 1,
-						mediumLevelStacks[Misc.random(4)], 1 + Misc.random(200));
+				displayReward(c,
+						mediumLevelReward[Misc.random(13)], 1,
+						mediumLevelReward[Misc.random(13)], 1,
+						mediumLevelStacks[Misc.random(4)], 1 + Misc.random(200)
+				);
 				break;
 			case 1:
-				displayReward(c, mediemLevelReward[Misc.random(13)], 1,
-						mediumLevelStacks[Misc.random(4)],
-						1 + Misc.random(200), -1, 1);
+				displayReward(c,
+						mediumLevelReward[Misc.random(13)], 1,
+						mediumLevelStacks[Misc.random(4)], 1 + Misc.random(200)
+				);
 				break;
 			case 2:
-				displayReward(c, mediemLevelReward[Misc.random(13)], 1, -1, 1,
-						-1, 1);
+				displayReward(c, mediumLevelReward[Misc.random(13)], 1);
 				break;
 			}
 		} else if (clueLevel == 2) {
 			switch (chanceReward) {
 			case 0:
-				displayReward(c, highLevelReward[Misc.random(52)], 1,
+				displayReward(c,
 						highLevelReward[Misc.random(52)], 1,
-						highLevelStacks[Misc.random(5)], 1 + Misc.random(350));
+						highLevelReward[Misc.random(52)], 1,
+						highLevelStacks[Misc.random(5)], 1 + Misc.random(350)
+				);
 				break;
 			case 1:
-				displayReward(c, highLevelReward[Misc.random(52)], 1,
-						highLevelStacks[Misc.random(5)], 1 + Misc.random(350),
-						-1, 1);
+				displayReward(c,
+						highLevelReward[Misc.random(52)], 1,
+						highLevelStacks[Misc.random(5)], 1 + Misc.random(350)
+				);
 				break;
 			case 2:
-				displayReward(c, highLevelReward[Misc.random(52)], 1, -1, 1,
-						-1, 1);
+				displayReward(c, highLevelReward[Misc.random(52)];
 				break;
 			}
 		}
 	}
+	public static void displayReward(Client c, int item, int amount) {
+		displayReward(c, item, amount, -1, 1);
+	}
 
-	public static void displayReward(Client c, int item, int amount, int item2,
-			int amount2, int item3, int amount3) {
+	public static void displayReward(Client c, int item, int amount, int item2, int amount2) {
+		displayReward(c, item, amount, item2, amount2, -1, 1);
+	}
+
+	public static void displayReward(Client c, int item, int amount, int item2, int amount2, int item3, int amount3) {
 		int[] items = { item, item2, item3 };
 		int[] amounts = { amount, amount2, amount3 };
 		c.outStream.createFrameVarSizeWord(53);
