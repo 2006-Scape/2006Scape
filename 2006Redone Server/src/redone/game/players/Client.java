912a08c42377ef89d2cdce2a4d482b54dba2c7e3
Dark98 <DarkAidz98@gmail.com>
2019-10-28 00:25:30 +0000

    Read Comment
    Made firstTimeTutorial handle the inital login stuff instead of it being ran with the dialogue
    made the warning about the bank pin send for all players
    fixed weight calculation on first login(closes #109)

--- 2006Redone Server/src/redone/game/players/Client.java
+++ 2006Redone Server/src/redone/game/players/Client.java
@@ -648,19 +648,23 @@ public class Client extends Player {
 				playerLevel[playerFarming] = 1;
 				getPlayerAssistant().refreshSkill(playerFarming);
 			}
-			getPlayerAssistant().firstTimeTutorial();
 			if (tutorialProgress > 0 && tutorialProgress < 36 && Constants.TUTORIAL_ISLAND) {
 				getActionSender().sendMessage("@blu@Continue the tutorial from the last step you were on.@bla@");
 			}
 			if (tutorialProgress > 35) {
 				getPlayerAssistant().sendSidebars();
-				getItemAssistant().sendWeapon(playerEquipment[playerWeapon], ItemAssistant.getItemName(playerEquipment[playerWeapon]));
+				Weight.updateWeight(this);
 				getActionSender().sendMessage("Welcome to @blu@" + Constants.SERVER_NAME + "@bla@ - we are currently in Server Stage v@blu@" + Constants.TEST_VERSION + "@bla@.");
 				getActionSender().sendMessage("@red@Did you know?@bla@ We're open source! Pull requests are welcome");
 				getActionSender().sendMessage("Source code at github.com/dginovker/2006rebotted");
 				getActionSender().sendMessage("Welcome to the Beta! A reset will occur before main release -");
-				getActionSender().sendMessage("Welcome to the 2006rebotted beta! Join our Discord: discord.gg/4zrA2Wy");
+				getActionSender().sendMessage("Join our Discord: discord.gg/4zrA2Wy");
+				if (!hasBankpin) {
+					getActionSender().sendMessage("You do not, have a bank pin it is highly recommened you set one.");
+				}
 			}
+			getPlayerAssistant().firstTimeTutorial();
+			getItemAssistant().sendWeapon(playerEquipment[playerWeapon], ItemAssistant.getItemName(playerEquipment[playerWeapon]));
 			for (int i = 0; i < 25; i++) {
 				getActionSender().setSkillLevel(i, playerLevel[i], playerXP[i]);
 				getPlayerAssistant().refreshSkill(i);
@@ -691,7 +695,6 @@ public class Client extends Player {
 				getPlayerAssistant().sendConfig(504, 0);
 				getPlayerAssistant().sendConfig(173, 0);
 			}
-			Weight.updateWeight(this);
 
 			getPlayList().fixAllColors();
 			getPlayerAction().setAction(false);
