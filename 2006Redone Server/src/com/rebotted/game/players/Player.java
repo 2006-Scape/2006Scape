18650aa0ca78db80e3ad073e3be7c1b765916cb1
dginovker <dcress01@uoguelph.ca>
2019-12-17 15:10:31 -0500

    Fix bank packet exploit + fishing guild banking

--- 2006Redone Server/src/com/rebotted/game/players/Player.java
+++ 2006Redone Server/src/com/rebotted/game/players/Player.java
@@ -1757,6 +1757,7 @@ public abstract class Player {
                 isInArea(2530,4725,2550,4705) || //Phasmatys
 				isInArea(2834, 10215, 2841, 10204) || // Keldagrim
 				isInArea(2379, 4453, 2386, 4462) || // Zanaris
+				isInArea(2582, 3423, 2591, 3417) || //Fishing Guild
 				false;
 	}
 
