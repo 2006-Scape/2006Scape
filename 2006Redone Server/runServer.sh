174bdf5bfd7a74e208991a00975bc8c59c33ffb9
Daniel Ginovker <dcress01@uoguelph.ca>
2020-02-19 08:46:42 -0500

    Make farming not nuke & latest build (#378)
    * Farming Skill, Orb Charging, Battle Staff Creation, etc
    
    - Farming Skill Added
    - Orb charging implemented
    - Battle staff creation implemented
    - Cleaned up some code
    - Stuck command will now tele you from further out in wildy if you are not in combat
    - Fixed an issue with amulet of glory
    - Added forcechats for npcs at Gnome Agility Course, and for Cows, Ducks and Sheeps just like in actual osrs
    - Added base for God Book preaching
    
    * Remove setting farm level to 1
    
    * Fix runServer.sh, patch farming skill nuke and latest build :)
    
    Co-authored-by: Mr Extremez <aeggers90@hotmail.com>

--- 2006Redone Server/runServer.sh
+++ 2006Redone Server/runServer.sh
@@ -2,5 +2,5 @@ echo NOTE: This program is meant to be run by the live server admin only!
 echo You probably don\'t want to run this, even if you\'re developing locally!
 for i in {0..50}
 do
-    java -jar build/libs/2006Redone Server.jar
+    java -jar "build/libs/2006Redone Server.jar"
 done
