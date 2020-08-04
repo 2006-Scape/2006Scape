66452ba01fb21e51e79234eeda4febc4ac180147
Sandro Coutinho <sandro@farrelltech.org>
2020-08-04 21:12:31 +0100

    [TASK] Fixed client source

--- 2006Redone Client/src/main/java/Class47.java
+++ 2006Redone Client/src/main/java/Class47.java
@@ -1,6 +1,6 @@
 // Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
 // Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
+// Decompiler options: packimports(3)
 
 final class Class47 {
 
