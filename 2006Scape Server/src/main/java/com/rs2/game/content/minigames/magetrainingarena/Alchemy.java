2ea399456859722b0406aa3ce9588f4b97b257a2
RedSparr0w <RedSparr0w@users.noreply.github.com>
2021-09-30 15:54:08 +1300

    Create Alchemy.java

---
+++ 2006Scape Server/src/main/java/com/rs2/game/content/minigames/magetrainingarena/Alchemy.java
@@ -0,0 +1,24 @@
+package com.rs2.game.content.minigames.magetrainingarena;
+
+import com.rs2.game.players.Player;
+
+public class Alchemy {
+    /* ITEMS */
+
+    /* OBJECTS */
+    // 10734 - Coin Collector
+    // 10783 - 1st Cupboard (+2 for the next 7 cupboards)
+
+    /* INTERFACES */
+    // 15892 - Alchemy training arena interface
+
+	private final Player player;
+
+	public Alchemy(Player c) {
+		this.player = c;
+	}
+
+
+	public void alchItem(int itemID, int spellID) {
+	}
+}
