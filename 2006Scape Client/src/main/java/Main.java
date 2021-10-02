f0eba8e45dd769458725ff134cb87fd07d085783
Danial <admin@redsparr0w.com>
2021-10-02 20:07:21 +1300

    Simplify

--- 2006Scape Client/src/main/java/Main.java
+++ 2006Scape Client/src/main/java/Main.java
@@ -9,28 +9,55 @@ public final class Main {
 
 	If you want to run the client locally, the easiest way to do that is run the class "Client.java" instead!
 
-	If you REALLY want to use this class, add two random program arguments.
+	If you REALLY want to use this class, add program arguments "-s localhost".
 	But seriously, Client.java is just a copy-paste of this class and does it locally. Use that instead!
-
 
 	 */
 
 	public static void main(String[] args) {
-		ClientSettings.SERVER_IP = args.length > 0 ? args[0] : ClientSettings.SERVER_IP;
 		try {
+			// Process server/ip address to connect to
+			for (int i = 0; i < args.length; i++) {
+				if (args[i].startsWith("-") && (i + 1) < args.length  && !args[i + 1].startsWith("-")) {
+					switch(args[i]) {
+						case "-s":
+						case "-server":
+						case "-ip":
+							ClientSettings.SERVER_IP = args[++i];
+							break;
+					}
+				}
+			}
+
 			Game game = new Game();
+
+			// Process other arguments
+			for (int i = 0; i < args.length; i++) {
+				if (args[i].startsWith("-") && (i + 1) < args.length  && !args[i + 1].startsWith("-")) {
+					switch(args[i]) {
+						case "-u":
+						case "-user":
+						case "-username":
+							game.myUsername = args[++i];
+							break;
+						case "-p":
+						case "-pass":
+						case "-password":
+							game.myPassword = args[++i];
+							break;
+					}
+				}
+			}
+
 			Game.nodeID = 10;
 			Game.portOff = 0;
 			Game.setHighMem();
 			Game.isMembers = true;
 			Signlink.storeid = 32;
 			Signlink.startpriv(InetAddress.getLocalHost());
-			game.myUsername = args.length > 1 ? args[1] : "";
-			game.myPassword = args.length > 2 ? args[2] : "";
 			game.createClientFrame(503, 765);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
