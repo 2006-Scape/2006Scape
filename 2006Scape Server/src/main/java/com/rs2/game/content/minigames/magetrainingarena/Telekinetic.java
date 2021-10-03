57175a126a2ac5ce6e8563747b9c51167c403648
RedSparr0w <RedSparr0w@users.noreply.github.com>
2021-10-04 10:53:06 +1300

    Add mazes initial

--- 2006Scape Server/src/main/java/com/rs2/game/content/minigames/magetrainingarena/Telekinetic.java
+++ 2006Scape Server/src/main/java/com/rs2/game/content/minigames/magetrainingarena/Telekinetic.java
@@ -9,6 +9,37 @@ import com.rs2.world.Boundary;
 
 public class Telekinetic {
 
+    public static enum Maze {
+        MAZE_0(3338, 9705, 3347, 9714, 0,
+            3343, 9705,
+            3347, 9714),
+        MAZE_1(3366, 9711, 3375, 9720, 0,
+            3375, 9715,
+            3367, 9720),
+        MAZE_2(3338, 9675, 3347, 9684, 0,
+            3343, 9680,
+            3342, 9684),
+        MAZE_3(3369, 9673, 3378, 9682, 0,
+            3373, 9678,
+            3375, 9682);
+
+        public int minX, minY, maxX, maxY, height, startX, startY, endX, endY;
+        public Boundary mazeArea;
+
+        private Maze(int minX, int minY, int maxX, int maxY, int height, int startX, int startY, int endX, int endY) {
+            this.minX = minX;
+            this.minY = minY;
+            this.maxX = maxX;
+            this.maxY = maxY;
+            this.height = height;
+            this.startX = startX;
+            this.startY = startY;
+            this.endX = endX;
+            this.endY = endY;
+            this.mazeArea = new Boundary(minX, maxX, minY, maxY, height);
+        }
+    }
+
 	private Player player;
 
 	public Telekinetic(Player c) {
