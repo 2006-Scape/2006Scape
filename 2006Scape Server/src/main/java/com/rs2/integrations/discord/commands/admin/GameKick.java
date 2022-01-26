162eb577269763d76a8ff742bfcf1a2efd14caed
Dark98 <Darkaidz98@gmail.com>
2022-01-26 07:27:16 +0000

    Added Support For User's Kicking Own Accounts Through Discord Bot

--- 2006Scape Server/src/main/java/com/rs2/integrations/discord/commands/admin/GameKick.java
+++ 2006Scape Server/src/main/java/com/rs2/integrations/discord/commands/admin/GameKick.java
@@ -7,25 +7,27 @@ import com.rs2.integrations.discord.JavaCord;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.util.Objects;
+
 public class GameKick implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
         if (event.getMessageContent().startsWith(JavaCord.commandPrefix + " gamekick")) {
-            if (event.getMessageAuthor().isServerAdmin()) {
-                String playerToKick = event.getMessageContent().replace( JavaCord.commandPrefix + " gamekick ", "");
-                for (Player player2 : PlayerHandler.players) {
-                    if (player2 != null) {
-                        if (player2.playerName.equalsIgnoreCase(playerToKick)) {
-                            Client c2 = (Client) player2;
-                            event.getChannel().sendMessage( playerToKick+ " was kicked by " + event.getMessageAuthor().getDisplayName() + ".");
+            String playerToKick = event.getMessageContent().replace( JavaCord.commandPrefix + " gamekick ", "");
+            for (Player player2 : PlayerHandler.players) {
+                if (player2 != null) {
+                    if (player2.playerName.equalsIgnoreCase(playerToKick)) {
+                        Client c2 = (Client) player2;
+                        if (event.getMessageAuthor().isServerAdmin() || Objects.equals(event.getMessageAuthor().getIdAsString(), c2.discordCode)) {
+                            event.getChannel().sendMessage(playerToKick + " was kicked by " + event.getMessageAuthor().getDisplayName() + ".");
                             c2.disconnected = true;
                             c2.logout(true);
-                            break;
+                        } else {
+                            event.getChannel().sendMessage("You do not have permission to perform this command");
                         }
+                        break;
                     }
                 }
-            } else {
-                event.getChannel().sendMessage("You do not have permission to perform this command");
             }
         }
     }
