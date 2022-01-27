54a3e5cdecc4fd7536818620898cdfddcf71ebe7
Josh Shippam <darkaidz98@gmail.com>
2022-01-27 01:32:17 +0000

    Various Changes (#523)
    * Discord Bot Update
    
    Updated JavaCord To Latest
    Changed Some Commands To No Longer Be World Specific
    Added An "AdminCommands" Command
    
    * Update Some Server Libs
    
    * Added Purge & Pin/Un-Pin To Commands Discord Bot
    
    * Fixed -dev & -no-crc Client Args
    
    * Add Discord User ID Linking Support(Useless For Now)
    
    * Added Support For User's Kicking Own Accounts Through Discord Bot
    
    * Merge Website Integration Stuff Into One Var

--- 2006Scape Server/src/main/java/com/rs2/integrations/discord/commands/Issues.java
+++ 2006Scape Server/src/main/java/com/rs2/integrations/discord/commands/Issues.java
@@ -1,6 +1,6 @@
 package com.rs2.integrations.discord.commands;
 
-import com.rs2.integrations.discord.JavaCord;
+import com.rs2.GameConstants;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
@@ -9,8 +9,10 @@ public class Issues implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
         Message message = event.getMessage();
-        if (message.getContent().equalsIgnoreCase(JavaCord.commandPrefix + " issues") || message.getContent().equalsIgnoreCase(JavaCord.commandPrefix+ " bugs")) {
-            event.getChannel().sendMessage("https://github.com/2006-Scape/2006Scape/issues");
+        if (message.getContent().equalsIgnoreCase("::issues") || message.getContent().equalsIgnoreCase("::bugs")) {
+            if (GameConstants.WORLD == 1) {
+                event.getChannel().sendMessage("https://github.com/2006-Scape/2006Scape/issues");
+            }
         }
     }
 }
