9fd4b470c8f5896c6dd1566d855de667d557567c
Dark98 <darkaidz98@gmail.com>
2022-08-22 05:44:54 +0100

    Setup GH Actions For Windows Single-Player

---
+++ SinglePlayer.bat
@@ -0,0 +1,6 @@
+:: Runs The Server & FileServer
+start cmd /k java -jar Server.jar -c ServerConfig.json
+:: Used To Wait 6 Seconds Before Running The Client
+PING localhost -n 6 >NUL 
+:: Starts The Client
+start cmd /k java -jar Client.jar -s localhost
