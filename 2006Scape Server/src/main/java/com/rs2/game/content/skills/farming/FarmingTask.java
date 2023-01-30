b9f0fb3ca3718c36aab0c2f02f29214694180414
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2023-01-30 10:59:54 -0500

    Converted FarmingTask to CycleEvent

--- 2006Scape Server/src/main/java/com/rs2/game/content/skills/farming/FarmingTask.java
+++ 2006Scape Server/src/main/java/com/rs2/game/content/skills/farming/FarmingTask.java
@@ -1,19 +1,23 @@
 package com.rs2.game.content.skills.farming;
 
+import com.rs2.event.CycleEvent;
+import com.rs2.event.CycleEventContainer;
 import com.rs2.game.players.Player;
-import com.rs2.tick.Tick;
 
-public class FarmingTask extends Tick {
-
-	private Player player;
+public class FarmingTask extends CycleEventContainer {
 
 	public FarmingTask(Player player) {
-		super(10);
-		this.player = player;
-	}
+		super("farming task".hashCode(), player, new CycleEvent() {
 
-	@Override
-	protected void execute() {
-		Farming.processCalc(player);
+			@Override
+			public void execute(CycleEventContainer container) {
+				Farming.processCalc(player);				
+			}
+
+			@Override
+			public void stop() {
+			}
+			
+		}, 10);
 	}
 }
