1db58a1ed692b89a624338075fb6c383a61e50e1
Dark98 <darkaidz98@gmail.com>
2023-02-13 19:14:30 +0000

    BattleStaffs.java IntelliJ Code Format

--- 2006Scape Server/src/main/java/com/rs2/game/content/skills/crafting/BattleStaffs.java
+++ 2006Scape Server/src/main/java/com/rs2/game/content/skills/crafting/BattleStaffs.java
@@ -10,98 +10,99 @@ import static com.rs2.game.content.StaticItemList.*;
 
 /**
  * Battle Staff making
+ *
  * @author Andrew (Mr Extremez)
  */
 public class BattleStaffs {
-	
-	public static enum battleStaffs {
 
-		AIR(AIR_ORB, 66, 138, AIR_BATTLESTAFF),
-		FIRE(FIRE_ORB, 62, 125, FIRE_BATTLESTAFF),
-		EARTH(EARTH_ORB, 58, 113, EARTH_BATTLESTAFF),
-		WATER(WATER_ORB, 54, 100, WATER_BATTLESTAFF);
+    public static enum battleStaffs {
 
-		private int orb, levelReq, xpRecieved, battlestaff;
+        AIR(AIR_ORB, 66, 138, AIR_BATTLESTAFF),
+        FIRE(FIRE_ORB, 62, 125, FIRE_BATTLESTAFF),
+        EARTH(EARTH_ORB, 58, 113, EARTH_BATTLESTAFF),
+        WATER(WATER_ORB, 54, 100, WATER_BATTLESTAFF);
 
-		private battleStaffs(final int orb, final int levelReq, final int xpRecieved, final int battlestaff) {
-			this.orb = orb;
-			this.levelReq = levelReq;
-			this.xpRecieved = xpRecieved;
-			this.battlestaff = battlestaff;
-		}
+        private int orb, levelReq, xpRecieved, battlestaff;
 
-		public int getOrb() {
-			return orb;
-		}
-		
-		public int getLevelReq() {
-			return levelReq;
-		}
-		
-		public int getXP() {
-			return xpRecieved;
-		}
+        private battleStaffs(final int orb, final int levelReq, final int xpRecieved, final int battlestaff) {
+            this.orb = orb;
+            this.levelReq = levelReq;
+            this.xpRecieved = xpRecieved;
+            this.battlestaff = battlestaff;
+        }
 
-		public int getBattlestaff() {
-			return battlestaff;
-		}
-	}
-	
-	public static void makeBattleStaff(Player player, int itemUsed, int usedWith) {
-		for (final battleStaffs b : battleStaffs.values()) {
-			if ((itemUsed == b.getOrb() && usedWith == BATTLESTAFF) || (itemUsed == BATTLESTAFF && usedWith == b.getOrb())) {
-				if (player.getItemAssistant().playerHasItem(BATTLESTAFF) && player.getItemAssistant().playerHasItem(b.getOrb())) {
-					if (player.isCrafting == true) {
-						return;
-					}
-					if (player.playerLevel[Constants.CRAFTING] < b.getLevelReq()) {
-						player.getPacketSender().sendMessage("You need level " + b.getLevelReq() + " crafting to do that.");
-						return;
-					}
-					player.getPacketSender().closeAllWindows();
-					player.isCrafting = true;
-					player.skillAmount = player.getItemAssistant().getItemAmount(b.getOrb());
-					CycleEventHandler.getSingleton().addEvent(player, new CycleEvent() {
-						@Override
-						public void execute(CycleEventContainer container) {
-							if (player.isCrafting == true && player != null) {
-								if (!player.getItemAssistant().playerHasItem(BATTLESTAFF, 1)) {
-									player.getPacketSender().sendMessage("You have run out of battlestaffs.");
-									container.stop();
-									return;
-								}
-								if (!player.getItemAssistant().playerHasItem(b.getOrb(), 1)) {
-									player.getPacketSender().sendMessage("You have run out of orbs.");
-									container.stop();
-									return;
-								}
-								if (player.skillAmount == 0) {
-									container.stop();
-									return;
-								}
-								player.getItemAssistant().deleteItem(BATTLESTAFF, 1);
-								player.getItemAssistant().deleteItem(b.getOrb(), 1);
-								player.getItemAssistant().addItem(b.getBattlestaff(), 1);
-								player.getPlayerAssistant().addSkillXP((int) b.getXP(), 6);
-								player.skillAmount--;
-								if (!player.getItemAssistant().playerHasItem(BATTLESTAFF) || !player.getItemAssistant().playerHasItem(b.getOrb())) {
-									container.stop();
-									return;
-								}
-							} else {
-								container.stop();
-							}
-						}
+        public int getOrb() {
+            return orb;
+        }
 
-						@Override
-						public void stop() {
-							player.isCrafting = false;
-						}
-					}, 2);
-				}
-			}
-		}
-	}
-	
+        public int getLevelReq() {
+            return levelReq;
+        }
+
+        public int getXP() {
+            return xpRecieved;
+        }
+
+        public int getBattlestaff() {
+            return battlestaff;
+        }
+    }
+
+    public static void makeBattleStaff(Player player, int itemUsed, int usedWith) {
+        for (final battleStaffs b : battleStaffs.values()) {
+            if ((itemUsed == b.getOrb() && usedWith == BATTLESTAFF) || (itemUsed == BATTLESTAFF && usedWith == b.getOrb())) {
+                if (player.getItemAssistant().playerHasItem(BATTLESTAFF) && player.getItemAssistant().playerHasItem(b.getOrb())) {
+                    if (player.isCrafting == true) {
+                        return;
+                    }
+                    if (player.playerLevel[Constants.CRAFTING] < b.getLevelReq()) {
+                        player.getPacketSender().sendMessage("You need level " + b.getLevelReq() + " crafting to do that.");
+                        return;
+                    }
+                    player.getPacketSender().closeAllWindows();
+                    player.isCrafting = true;
+                    player.skillAmount = player.getItemAssistant().getItemAmount(b.getOrb());
+                    CycleEventHandler.getSingleton().addEvent(player, new CycleEvent() {
+                        @Override
+                        public void execute(CycleEventContainer container) {
+                            if (player.isCrafting == true && player != null) {
+                                if (!player.getItemAssistant().playerHasItem(BATTLESTAFF, 1)) {
+                                    player.getPacketSender().sendMessage("You have run out of battlestaffs.");
+                                    container.stop();
+                                    return;
+                                }
+                                if (!player.getItemAssistant().playerHasItem(b.getOrb(), 1)) {
+                                    player.getPacketSender().sendMessage("You have run out of orbs.");
+                                    container.stop();
+                                    return;
+                                }
+                                if (player.skillAmount == 0) {
+                                    container.stop();
+                                    return;
+                                }
+                                player.getItemAssistant().deleteItem(BATTLESTAFF, 1);
+                                player.getItemAssistant().deleteItem(b.getOrb(), 1);
+                                player.getItemAssistant().addItem(b.getBattlestaff(), 1);
+                                player.getPlayerAssistant().addSkillXP((int) b.getXP(), 6);
+                                player.skillAmount--;
+                                if (!player.getItemAssistant().playerHasItem(BATTLESTAFF) || !player.getItemAssistant().playerHasItem(b.getOrb())) {
+                                    container.stop();
+                                    return;
+                                }
+                            } else {
+                                container.stop();
+                            }
+                        }
+
+                        @Override
+                        public void stop() {
+                            player.isCrafting = false;
+                        }
+                    }, 2);
+                }
+            }
+        }
+    }
+
 
 }
