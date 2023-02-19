81eacc4637b664adcd3b4d21b436ecb271c39d97
Josh Shippam <darkaidz98@gmail.com>
2023-02-19 04:25:46 +0000

    Fix Some More Full Helms

--- 2006Scape Server/src/main/java/com/rs2/game/items/ItemData.java
+++ 2006Scape Server/src/main/java/com/rs2/game/items/ItemData.java
@@ -143,7 +143,7 @@ public class ItemData {
 			"Armadyl helmet", "Berserker helm", "Archer helm", "Farseer helm",
 			"Warrior helm", "Void"};
 	/* Fullmask covers your entire head. */  
-	private static String[] fullmask = { "full helm(t)", "full helm(g)", "full helm", "mask", "Veracs helm",
+	private static String[] fullmask = { "full helm (t)", "full helm (g)", "full helm", "mask", "Veracs helm",
 			"Guthans helm", "Karils coif", "mask", "Torags helm", "Void", "helmet",
 			"sallet", "Facemask", "Bearhead"};
 
