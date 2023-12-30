e9ea92722148d464f3e932616cd17dacc9e18872
ipkpjersi <ipkpjersi@gmail.com>
2023-12-30 16:06:55 -0500

    Improved slayer points message

--- 2006Scape Server/src/main/java/com/rs2/game/content/skills/slayer/Slayer.java
+++ 2006Scape Server/src/main/java/com/rs2/game/content/skills/slayer/Slayer.java
@@ -436,8 +436,8 @@ public class Slayer {
 			return;
 		}
 		if (c.slayerPoints < 30) {
-			c.getPacketSender().sendMessage("This requires atleast 30 slayer points, which you don't have.");
-			c.getDialogueHandler().sendNpcChat1("This requires atleast 30 slayer points, which you don't have.", c.npcType,	NpcHandler.getNpcListName(c.talkingNpc));
+			c.getPacketSender().sendMessage("This requires 30 slayer points, which you don't have.");
+			c.getDialogueHandler().sendNpcChat1("This requires 30 slayer points, which you don't have.", c.npcType,	NpcHandler.getNpcListName(c.talkingNpc));
 			c.nextChat = 0;
 			return;
 		}
