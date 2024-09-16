542820da8cb2f881d5c3e5542a1a46bbe0a67506
ipkpjersi <ipkpjersi@gmail.com>
2024-09-16 19:37:41 -0400

    Changed comment to TODO

--- 2006Scape Client/src/main/java/Game.java
+++ 2006Scape Client/src/main/java/Game.java
@@ -2975,7 +2975,7 @@ public class Game extends RSApplet {
 		if (anInt1011 > 0) {
 			anInt1011--;
 		}
-		//Technically, this loop should be < 5 for authenticity, but until we reduce server inefficiencies
+		//TODO: Technically, this loop should be < 5 for authenticity, but until we reduce server inefficiencies
 		//(for example killing a bunch of cows results in client lag, 
 		//likely from all the items on the ground for example), < 100 is fine. 
 		//OSRS uses < 100 and there are no drawbacks from having this < 100.
